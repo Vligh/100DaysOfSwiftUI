1820706aeeebf2a498cd5ff62d8a546b3ecee54b
Vitali Tatarintev <Kalastiuz@gmail.com>
2019-10-27 09:42:47 +0100

    Create a Landmarks project

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Vitaly Tatarintsev on 27/10/2019.
+//  Copyright © 2019 Vitali Tatarintev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Turtle Rock")
+      .font(.title)
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
