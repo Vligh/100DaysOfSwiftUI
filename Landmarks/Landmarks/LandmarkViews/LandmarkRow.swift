46c3ece4070c193d958f1e76f9c99ca405105fbf
Vitali Tatarintev <Kalastiuz@gmail.com>
2019-10-28 09:03:20 +0100

    Display a first landmark row

--- Landmarks/Landmarks/LandmarkViews/LandmarkRow.swift
+++ Landmarks/Landmarks/LandmarkViews/LandmarkRow.swift
@@ -9,13 +9,22 @@
 import SwiftUI
 
 struct LandmarkRow: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+  var landmark: Landmark
+
+  var body: some View {
+    HStack {
+      landmark.image
+        .resizable()
+        .frame(width: 50, height: 50)
+      Text(landmark.name)
+
+      Spacer()
     }
+  }
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
-    static var previews: some View {
-        LandmarkRow()
-    }
+  static var previews: some View {
+    LandmarkRow(landmark: landmarkData[0])
+  }
 }
