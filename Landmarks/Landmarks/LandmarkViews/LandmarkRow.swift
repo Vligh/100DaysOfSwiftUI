5934802ae7c1f222068149a9d00cae4833cfaf76
Vitali Tatarintev <Kalastiuz@gmail.com>
2019-10-28 07:19:08 +0100

    Add Resources for Landmarks and update the files for 2nd tutorial

---
+++ Landmarks/Landmarks/LandmarkViews/LandmarkRow.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Vitaly Tatarintsev on 28/10/2019.
+//  Copyright © 2019 Vitali Tatarintev. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
