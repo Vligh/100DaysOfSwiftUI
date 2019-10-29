d326447e4716286181a0833fbc25de7bddd613d0
Vitali Tatarintev <Kalastiuz@gmail.com>
2019-10-29 08:47:16 +0100

    Catch up with the tutorial differences

--- Landmarks/Landmarks/Models/Landmarks.swift
+++ Landmarks/Landmarks/Models/Landmarks.swift
@@ -10,6 +10,7 @@ struct Landmark: Hashable, Codable, Identifiable {
   var state: String
   var park: String
   var category: Category
+  var isFavorite: Bool
 
   var locationCoordinate: CLLocationCoordinate2D {
     CLLocationCoordinate2D(
@@ -22,6 +23,7 @@ struct Landmark: Hashable, Codable, Identifiable {
     case featured = "Featured"
     case lakes = "Lakes"
     case rivers = "Rivers"
+    case mountains = "Mountains"
   }
 }
 
