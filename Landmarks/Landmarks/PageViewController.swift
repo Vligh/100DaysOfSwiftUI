c40e3499fcd24a0e00bdb428e277c8cc3f5a6b42
Vitali Tatarintev <Kalastiuz@gmail.com>
2019-11-03 10:44:44 +0100

    Add a custom page control
    https://developer.apple.com/tutorials/swiftui/interfacing-with-uikit#add-a-custom-page-control

--- Landmarks/Landmarks/PageViewController.swift
+++ Landmarks/Landmarks/PageViewController.swift
@@ -9,8 +9,9 @@
 import SwiftUI
 import UIKit
 
-struct PageViewController : UIViewControllerRepresentable {
+struct PageViewController: UIViewControllerRepresentable {
   var controllers: [UIViewController]
+  @Binding var currentPage: Int
   
   func makeCoordinator() -> Coordinator {
     Coordinator(self)
@@ -21,17 +22,17 @@ struct PageViewController : UIViewControllerRepresentable {
       transitionStyle: .scroll,
       navigationOrientation: .horizontal)
     pageViewController.dataSource = context.coordinator
-      
+    pageViewController.delegate = context.coordinator
+    
     return pageViewController
   }
   
   func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
     pageViewController.setViewControllers(
-      [controllers[0]], direction: .forward, animated: true
-    )
+      [controllers[currentPage]], direction: .forward, animated: true)
   }
   
-  class Coordinator: NSObject, UIPageViewControllerDataSource {
+  class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
     var parent: PageViewController
     
     init(_ pageViewController: PageViewController) {
@@ -40,32 +41,34 @@ struct PageViewController : UIViewControllerRepresentable {
     
     func pageViewController(
       _ pageViewController: UIPageViewController,
-      viewControllerBefore viewController: UIViewController
-    ) -> UIViewController? {
+      viewControllerBefore viewController: UIViewController) -> UIViewController? {
       guard let index = parent.controllers.firstIndex(of: viewController) else {
         return nil
       }
-      
       if index == 0 {
         return parent.controllers.last
       }
-      
       return parent.controllers[index - 1]
     }
     
     func pageViewController(
       _ pageViewController: UIPageViewController,
-      viewControllerAfter viewController: UIViewController
-    ) -> UIViewController? {
+      viewControllerAfter viewController: UIViewController) -> UIViewController? {
       guard let index = parent.controllers.firstIndex(of: viewController) else {
         return nil
       }
-      
       if index + 1 == parent.controllers.count {
         return parent.controllers.first
       }
-      
       return parent.controllers[index + 1]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+      if completed,
+        let visibleViewController = pageViewController.viewControllers?.first,
+        let index = parent.controllers.firstIndex(of: visibleViewController) {
+        parent.currentPage = index
+      }
     }
   }
 }
