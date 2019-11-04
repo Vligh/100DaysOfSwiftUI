9e8682b12b36187c3f66a1c20cd8b1a110e73ce5
Vitali Tatarintev <Kalastiuz@gmail.com>
2019-11-04 07:13:11 +0100

    Add a watchOS Target
    https://developer.apple.com/tutorials/swiftui/creating-a-watchos-app#add-a-watchos-target

---
+++ Landmarks/WatchLandmarks Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  WatchLandmarks Extension
+//
+//  Created by Vitali Tatarintev on 04.11.19.
+//  Copyright © 2019 Vitali Tatarintev. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ Landmarks/WatchLandmarks Extension/NotificationView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationView.swift
+//  WatchLandmarks Extension
+//
+//  Created by Vitali Tatarintev on 04.11.19.
+//  Copyright © 2019 Vitali Tatarintev. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
+#endif
