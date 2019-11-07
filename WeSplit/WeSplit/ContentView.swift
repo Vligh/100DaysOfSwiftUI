407a26415201240e19df2b241a088b2c866a0d94
Vitali Tatarintev <Kalastiuz@gmail.com>
2019-11-07 07:29:24 +0100

    Convert number of people into text field

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
   @State private var checkAmount = ""
-  @State private var numberOfPeople = 2
+  @State private var numberOfPeople = "2"
   @State private var tipPercentage = 2
   
   let tipPercentages = [10, 15, 20, 25, 0]
@@ -24,8 +24,9 @@ struct ContentView: View {
     
     return grandTotal
   }
+
   var totalPerPerson: Double {
-    let peopleCount = Double(numberOfPeople + 2)
+    let peopleCount = Double(numberOfPeople) ?? 2
 
     let amountPerPerson = totalAmount / peopleCount
     
@@ -39,11 +40,8 @@ struct ContentView: View {
           TextField("Amount", text: $checkAmount)
             .keyboardType(.decimalPad)
           
-          Picker("Number of people", selection: $numberOfPeople) {
-            ForEach(2 ..< 100) {
-              Text("\($0) people")
-            }
-          }
+          TextField("Number of people", text: $numberOfPeople)
+            .keyboardType(.numberPad)
         }
         
         Section(header: Text("How much tip do you want to leave?")) {
