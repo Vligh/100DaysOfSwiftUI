df8a24e5e7a912b58fbeba8c8c37b820ef6f371e
Vitali Tatarintev <Kalastiuz@gmail.com>
2019-11-08 08:25:26 +0100

    Build the app interface

--- Unit2Unit/Unit2Unit/ContentView.swift
+++ Unit2Unit/Unit2Unit/ContentView.swift
@@ -9,8 +9,44 @@
 import SwiftUI
 
 struct ContentView: View {
+  let temperatureUnits = ["Celsius", "Fahrenheit", "Kelvin"]
+  
+  @State private var sourceValue = "0"
+  @State private var targetValue = "0"
+  @State private var sourceUnit = 0
+  @State private var targetUnit = 1
+  
   var body: some View {
-    Text("Hello World")
+    NavigationView {
+      Form {
+        Section(header: Text("Choose what do you want to convert")) {
+          TextField("Source", text: $sourceValue)
+            .keyboardType(.decimalPad)
+          
+          Picker("Source unit", selection: $sourceUnit) {
+            ForEach(0 ..< temperatureUnits.count) {
+              Text(self.temperatureUnits[$0])
+            }
+          }
+          .pickerStyle(SegmentedPickerStyle())
+        }
+        
+        Section(header: Text("Choose what do you want to see as a result")) {
+          Picker("Target unit", selection: $targetUnit) {
+            ForEach(0 ..< temperatureUnits.count) {
+              Text(self.temperatureUnits[$0])
+            }
+          }
+          .pickerStyle(SegmentedPickerStyle())
+        }
+        
+        Section {
+          Text("0 Celsius is 32 Fahrenheit")
+        }
+      }
+
+      .navigationBarTitle("Unit2Unit")
+    }
   }
 }
 
