85f47c58d06b6245dbf83a2a603ef34686f4bcb3
Vitali Tatarintev <Kalastiuz@gmail.com>
2019-11-12 09:22:35 +0100

    Implement game logic

--- RPSLS/RPSLS/ContentView.swift
+++ RPSLS/RPSLS/ContentView.swift
@@ -13,14 +13,49 @@ struct ContentView: View {
   @State private var gameResultText = ""
   @State private var gameResultDescription = ""
   
+  let whatDoesItDo = [
+    "Rock": ["Scissors": "crushes", "Lizard": "crushes"],
+    "Paper": ["Rock": "covers", "Spock": "disproves"],
+    "Scissors": ["Paper": "cuts", "Lizard": "decapitates"],
+    "Lizard": ["Paper": "eats", "Spock": "poisons"],
+    "Spock": ["Rock": "vaporizes", "Scissors": "smashes"]
+  ]
+  let doesWin = [
+    "Rock": ["Paper": false, "Scissors": true, "Lizard": true, "Spock": false],
+    "Paper": ["Rock": true, "Scissors": false, "Lizard": false, "Spock": true],
+    "Scissors": ["Rock": false, "Paper": true, "Lizard": true, "Spock": false],
+    "Lizard": ["Rock": false, "Paper": true, "Scissors": false, "Spock": true],
+    "Spock": ["Rock": true, "Paper": false, "Scissors": true, "Lizard": false]
+  ]
+
   var body: some View {
     VStack {
       Text("Make your move")
         .padding()
-      
+
       ForEach(0 ..< moves.count) { move in
         Button(action: {
-          // Do something
+          let aiMoveIndex = Int.random(in: 0 ..< self.moves.count)
+          let aiMove = self.moves[aiMoveIndex]
+          let userMove = self.moves[move]
+          var winner = userMove
+          var loser = aiMove
+          var action = ""
+
+          if userMove == aiMove {
+            self.gameResultText = "Draw!"
+            action = "does nothing to"
+          } else if self.doesWin[userMove]![aiMove]! {
+            self.gameResultText = "You won!"
+            action = self.whatDoesItDo[userMove]![aiMove]!
+          } else {
+            self.gameResultText = "You lost!"
+            winner = aiMove
+            loser = userMove
+            action = self.whatDoesItDo[aiMove]![userMove]!
+          }
+
+          self.gameResultDescription = "\(winner) \(action) \(loser)"
         }) {
           Text(self.moves[move])
             .frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
@@ -30,8 +65,10 @@ struct ContentView: View {
       }
       
       Text(gameResultText)
+        .frame(width: 200)
         .padding()
       Text(gameResultDescription)
+        .frame(width: 300)
         .padding()
       
       Spacer()
