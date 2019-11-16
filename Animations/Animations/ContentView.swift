86bea29fbde359e9089222c48a7b7cbfcdc0ece3
Vitali Tatarintev <Kalastiuz@gmail.com>
2019-11-16 08:40:19 +0100

    Try different animations

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -13,15 +13,31 @@ struct ContentView: View {
 
   var body: some View {
     Button("Tap Me") {
-      self.animationAmount += 1
+      // self.animationAmount += 1
     }
     .padding(50)
     .background(Color.red)
     .foregroundColor(.white)
     .clipShape(Circle())
-    .scaleEffect(animationAmount)
-    .blur(radius: (animationAmount - 1) * 3)
-    .animation(.default)
+    .overlay(
+      Circle()
+        .stroke(Color.red)
+        .scaleEffect(animationAmount)
+        .opacity(Double(2 - animationAmount))
+        .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: false))
+    )
+    //.scaleEffect(animationAmount)
+    //.blur(radius: (animationAmount - 1) * 3)
+    //.animation(.default)
+    //.animation(.easeOut)
+    //.animation(.interpolatingSpring(stiffness: 50, damping: 1))
+    //.animation(.easeInOut(duration: 2))
+    //.animation(Animation.easeInOut(duration: 2).delay(1))
+    //.animation(Animation.easeInOut(duration: 1).repeatCount(3, autoreverses: true))
+    //.animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true))
+    .onAppear {
+      self.animationAmount = 2
+    }
   }
 }
 
