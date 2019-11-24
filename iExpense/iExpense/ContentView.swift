414fad2dc9858cea92bd3ebfddbc66ab4bc84833
Vitali Tatarintev <Kalastiuz@gmail.com>
2019-11-24 09:10:58 +0100

    Add Edit/Done button to nav bar

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -66,7 +66,7 @@ struct ContentView: View {
         .onDelete(perform: removeItems)
       }
       .navigationBarTitle("iExpense")
-      .navigationBarItems(trailing:
+      .navigationBarItems(leading: EditButton(), trailing:
         Button(action: {
           self.showingAddExpense = true
         }) {
