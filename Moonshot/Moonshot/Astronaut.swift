aeb1cc20a172f3dd5dbbc308afeb14bd09d6e177
Vitali Tatarintev <Kalastiuz@gmail.com>
2019-11-25 08:56:47 +0100

    Define decodable bundle to load a list of astronauts

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Vitali Tatarintev on 25.11.19.
+//  Copyright © 2019 Vitali Tatarintev. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+  let id: String
+  let name: String
+  let description: String
+}
