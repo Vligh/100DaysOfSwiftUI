ae78df14b156268de4dc0a380e5f0b2c7729b198
Vitali Tatarintev <Kalastiuz@gmail.com>
2019-11-26 09:08:15 +0100

    Display a list of Astronaut missions

--- Moonshot/Moonshot/AstronautView.swift
+++ Moonshot/Moonshot/AstronautView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct AstronautView: View {
   let astronaut: Astronaut
+  let missions: [Mission]
 
   var body: some View {
     GeometryReader { geometry in
@@ -23,10 +24,44 @@ struct AstronautView: View {
           Text(self.astronaut.description)
             .padding()
             .layoutPriority(1)
+
+          VStack(alignment: .leading) {
+            Text("Missions")
+              .font(.headline)
+
+            ForEach(self.missions) { mission in
+              HStack {
+                Image(mission.image)
+                .resizable()
+                .scaledToFit()
+                  .frame(width: 44, height: 44)
+
+                Text(mission.displayName)
+
+                Spacer()
+              }
+            }
+          }
+        .padding()
         }
       }
     }
     .navigationBarTitle(Text(astronaut.name), displayMode: .inline)
+  }
+
+  init(astronaut: Astronaut) {
+    self.astronaut = astronaut
+    let allMissions: [Mission] = Bundle.main.decode("missions.json")
+
+    var matches = [Mission]()
+
+    for mission in allMissions {
+      if let _ = mission.crew.first(where: { $0.name == astronaut.id }) {
+        matches.append(mission)
+      }
+    }
+
+    self.missions = matches
   }
 }
 
