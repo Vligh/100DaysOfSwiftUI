b625aaa04bebd4deae81fa85e1be40a5a377975b
Vitali Tatarintev <Kalastiuz@gmail.com>
2019-12-08 09:39:10 +0100

    Create a blank Cupcake Corner project

---
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Vitali Tatarintev on 08.12.19.
+//  Copyright © 2019 Vitali Tatarintev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello World")
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ CupcakeCorner/README.md
@@ -0,0 +1,6 @@
+# Cupcake Corner
+
+A multi-screen app for ordering cupcakes
+
+More info [here](https://www.hackingwithswift.com/books/ios-swiftui/cupcake-corner-introduction)
+
