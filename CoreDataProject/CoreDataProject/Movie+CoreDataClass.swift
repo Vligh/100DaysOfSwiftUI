e6bc7dd9282e4adb597246c7d04f1c81cf14ec26
Vitali Tatarintev <Kalastiuz@gmail.com>
2019-12-14 09:23:50 +0100

    Add a Movie entity with a custom class

---
+++ CoreDataProject/CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Vitali Tatarintev on 14.12.19.
+//  Copyright © 2019 Vitali Tatarintev. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
