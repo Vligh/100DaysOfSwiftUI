fd1782c57e16c8f6d007414b90dab20e69178e45
Vitali Tatarintev <Kalastiuz@gmail.com>
2019-12-16 07:34:57 +0100

    Describe a Friend structure

---
+++ FriendFace/FriendFace/Friend.swift
@@ -0,0 +1,14 @@
+//
+//  Friend.swift
+//  FriendFace
+//
+//  Created by Vitali Tatarintev on 16.12.19.
+//  Copyright © 2019 Vitali Tatarintev. All rights reserved.
+//
+
+import Foundation
+
+struct Friend: Codable {
+  var id = UUID()
+  var name: String
+}
