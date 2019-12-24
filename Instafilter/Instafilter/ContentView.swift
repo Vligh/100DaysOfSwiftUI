e45eeff2185ee8cebeeb7fd35663965994e31cc9
Vitali Tatarintev <Kalastiuz@gmail.com>
2019-12-24 10:12:15 +0100

    Create a blank Instafilter project

---
+++ Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Vitali Tatarintev on 24.12.19.
+//  Copyright © 2019 Vitali Tatarintev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello World")
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ Instafilter/README.md
@@ -0,0 +1,6 @@
+# Instafilter
+
+An app that lets the user import photos from their library, then modify them using various image effects
+
+More info [here](https://www.hackingwithswift.com/books/ios-swiftui/instafilter-introduction).
+
