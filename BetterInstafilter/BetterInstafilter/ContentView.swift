c9d15c5ade7ca5d6d31dbdce44716e2796c89cdb
Vitali Tatarintev <Kalastiuz@gmail.com>
2019-12-25 12:08:49 +0100

    Build an app layout

--- BetterInstafilter/BetterInstafilter/ContentView.swift
+++ BetterInstafilter/BetterInstafilter/ContentView.swift
@@ -9,13 +9,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello World")
+  @State private var image: Image?
+  @State private var filterIntensity = 0.5
+
+  var body: some View {
+    NavigationView {
+      VStack {
+        ZStack {
+          Rectangle()
+            .fill(Color.secondary)
+
+          if image != nil {
+            image?
+              .resizable()
+              .scaledToFit()
+          } else {
+            Text("Tap to select a picture")
+              .foregroundColor(.white)
+              .font(.headline)
+          }
+        }
+        .onTapGesture {
+          // select an image
+        }
+
+        HStack {
+          Text("Intensity")
+          Slider(value: self.$filterIntensity)
+        }
+        .padding(.vertical)
+
+        HStack {
+          Button("Change Filter") {
+            // change filter
+          }
+
+          Spacer()
+
+          Button("Save") {
+            // save the picture
+          }
+        }
+      }
+      .padding([.horizontal, .bottom])
+      .navigationBarTitle("Instafilter")
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
