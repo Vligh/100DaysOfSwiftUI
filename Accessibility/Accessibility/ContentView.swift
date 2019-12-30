821e124cca893d106c30e96378d79e6cace123b2
Vitali Tatarintev <Kalastiuz@gmail.com>
2019-12-30 09:49:08 +0100

    Create a blank Accessibility project

---
+++ Accessibility/Accessibility/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Accessibility
+//
+//  Created by Vitali Tatarintev on 30.12.19.
+//  Copyright © 2019 Vitali Tatarintev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello World")
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ Accessibility/README.md
@@ -0,0 +1,7 @@
+# Accessibility
+
+A playground app to practice building accessibility functionality
+
+
+More info [here](https://www.hackingwithswift.com/books/ios-swiftui/accessibility-introduction).
+
