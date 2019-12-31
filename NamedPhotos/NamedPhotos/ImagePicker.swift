d0690837248c7894638a62e4eb0339567cc02b88
Vitali Tatarintev <Kalastiuz@gmail.com>
2019-12-31 11:55:20 +0100

    Add the ImagePicker view

---
+++ NamedPhotos/NamedPhotos/ImagePicker.swift
@@ -0,0 +1,46 @@
+//
+//  ImagePicker.swift
+//  NamedPhotos
+//
+//  Created by Vitali Tatarintev on 31.12.19.
+//  Copyright © 2019 Vitali Tatarintev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+  @Environment(\.presentationMode) var presentationMode
+  @Binding var image: UIImage?
+  var onDismiss: () -> Void
+
+  class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var parent: ImagePicker
+
+    init(_ parent: ImagePicker) {
+      self.parent = parent
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+      if let uiImage = info[.originalImage] as? UIImage {
+        parent.image = uiImage
+      }
+
+      parent.presentationMode.wrappedValue.dismiss()
+      parent.onDismiss()
+    }
+  }
+
+  func makeCoordinator() -> Coordinator {
+    Coordinator(self)
+  }
+
+  func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIViewControllerType {
+    let picker = UIImagePickerController()
+    picker.delegate = context.coordinator
+    return picker
+  }
+
+  func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+
+  }
+}
