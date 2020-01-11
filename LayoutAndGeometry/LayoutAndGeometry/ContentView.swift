4bde19c0aff37188bc16080d522fc87e17a4575f
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-01-11 11:39:57 +0100

    Alignment and alignment guides
    https://www.hackingwithswift.com/books/ios-swiftui/alignment-and-alignment-guides

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -10,7 +10,40 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Text("Hello, World!")
+
+//    // Frame alignment
+//    Text("Live long and prosper")
+//      .frame(width: 300, height: 300, alignment: .topLeading)
+
+//    // Stack alignment
+//    HStack(alignment: .lastTextBaseline) {
+//      Text("Live").font(.caption)
+//      Text("long")
+//      Text("and").font(.title)
+//      Text("prosper").font(.largeTitle)
+//    }
+
+
+//    // Custom alignment
+//    VStack(alignment: .leading) {
+//      Text("Hello, world!")
+//        .alignmentGuide(.leading) { d in d[.trailing] }
+//      Text("This is a longer line of text")
+//    }
+//    .background(Color.red)
+//    .frame(width: 400, height: 400)
+//    .background(Color.blue)
+
+    // More on custom alignment
+    VStack(alignment: .leading) {
+      ForEach(0..<10) { position in
+        Text("Number \(position)")
+          .alignmentGuide(.leading) { _ in CGFloat(position) * -10 }
+      }
+    }
+    .background(Color.red)
+    .frame(width: 400, height: 400)
+    .background(Color.blue)
   }
 }
 
