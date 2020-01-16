f424ac76a9f4a0a2dbc91339f87f44858573baba
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-01-16 09:53:12 +0100

    Allow Add to Favorites

--- SnowSeeker/SnowSeeker/ResortView.swift
+++ SnowSeeker/SnowSeeker/ResortView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct ResortView: View {
   @Environment(\.horizontalSizeClass) var sizeClass
+  @EnvironmentObject var favorites: Favorites
 
   @State private var selectedFacility: Facility?
 
@@ -58,6 +59,15 @@ struct ResortView: View {
         }
         .padding(.horizontal)
       }
+
+      Button(favorites.contains(resort) ? "Remove from Favorites" : "Add to Favorites") {
+        if self.favorites.contains(self.resort) {
+          self.favorites.remove(self.resort)
+        } else {
+          self.favorites.add(self.resort)
+        }
+      }
+      .padding()
     }
     .navigationBarTitle(Text("\(resort.name), \(resort.country)"), displayMode: .inline)
     .alert(item: $selectedFacility) { facility in
